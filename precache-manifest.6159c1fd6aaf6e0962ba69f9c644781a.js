7e64bc6cf3fe181777f19338aaf977f09f1bb72c
Semion Shishlovi <ruddch@yandex-team.ru>
2019-06-02 17:24:05 +0300

    Updates

---
+++ precache-manifest.6159c1fd6aaf6e0962ba69f9c644781a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32bebb602b915d156f22630885d22ebc",
+    "url": "/multiselect/index.html"
+  },
+  {
+    "revision": "b28cf15aae297c546da4",
+    "url": "/multiselect/static/css/main.a110c311.chunk.css"
+  },
+  {
+    "revision": "076c53b55ab069e224e9",
+    "url": "/multiselect/static/js/2.259ab005.chunk.js"
+  },
+  {
+    "revision": "b28cf15aae297c546da4",
+    "url": "/multiselect/static/js/main.4188147d.chunk.js"
+  },
+  {
+    "revision": "25494ef56f38c2e41274",
+    "url": "/multiselect/static/js/runtime~main.fab4da27.js"
+  }
+]);
